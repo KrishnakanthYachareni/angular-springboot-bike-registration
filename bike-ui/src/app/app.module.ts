807c3f2a20c72231fd1ec7f39982a3fc68f583fe
Krishnakanth Yachareni <Krishnakanth_Yachareni@epam.com>
2018-06-27 22:59:56 +0530

    added

---
+++ bike-ui/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BikeService } from './services/bike.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    ViewRegistrationComponent,
+    CallbackComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ], 
+  providers: [BikeService, AuthService,AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
